6ce5646e1716b1e07e0b4a241a535c97f6908dfc
SNVTahoe <41130062+SNVtahoe@users.noreply.github.com>
2019-02-08 17:39:18 -0800

    Updates

--- app/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-require('./index.css');
-import App from './components/App';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
